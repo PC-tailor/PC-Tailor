f15c052b6bbf6e6eb7051026ceccf68d0f1b9d27
PC tailor <PCtailor2025@gmail.com>
2025-01-03 22:01:47 +0200

    Eka kunnon versio

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert('Tervetuloa sivustolleni');
-});
+
+// Funktio valikon avaamiseksi/sulkemiseksi
+function toggleMenu() {
+        const menu = document.querySelector('.menu');
+        const overlay = document.getElementById('page-overlay');
+        const menuBar = document.querySelector('.menu-bar');
+        menu.classList.toggle('open');
+        menuBar.classList.toggle('open');
+        overlay.style.display = menu.classList.contains('open') ? 'block' : 'none';
+    }
+   
+   
